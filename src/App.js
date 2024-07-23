6c68ad14cb6defc068782202fe5b58abb28dc2aa
Abhir Dongare <abhirdongare24@gmail.com>
2024-07-23 08:41:49 -0700

    Cards and css

--- src/App.js
+++ src/App.js
@@ -1,51 +1,59 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { getAllItems, addItem, removeItem } from './modules/ApiHelpers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './App.css';
-import { useEffect, useState } from 'react';
-import { addItem, getAllItems, removeItem } from './modules/ApiHelpers';
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [cards, setCards] = useState([]);
 
   useEffect(() => {
     async function fetchItems() {
-      await handleAddItem();
-      await handleRemoveItem(1);
       const data = await getAllItems();
-      setItems(data);
+      setCards(data);
     }
 
     fetchItems();
-    /* handleAddItem();
-    handleRemoveItem(1); */
   }, []);
 
   const handleAddItem = async () => {
     const newItem = { name: 'New Item' };
     const addedItem = await addItem(newItem);
-    setItems([...items, addedItem]);
+    setCards([...cards, addedItem]);
   };
 
   const handleRemoveItem = async (id) => {
     await removeItem(id);
-    setItems(items.filter(item => item.id !== id));
+    setCards(cards.filter(item => item.id !== id));
   };
-  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='pageWrapper'>
+      <div className="header">
+        <h1>99th precinct roster</h1>
+        <button onClick={handleAddItem}>Add new</button>
+      </div>
+      <div className='listWrapper'>
+        <TransitionGroup className='groupWrapper' >
+          {cards.map(item => (
+            <CSSTransition key={item.id} timeout={500} classNames="card">
+              <div className="card">
+                <div className="container">
+                  <h2><b>{item.name}</b></h2>
+                  <div className="iconWrapper">
+                    <FontAwesomeIcon
+                      icon={faTrashAlt}
+                      className="deleteIcon"
+                      onClick={() => handleRemoveItem(item.id)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </div>
     </div>
   );
 }
